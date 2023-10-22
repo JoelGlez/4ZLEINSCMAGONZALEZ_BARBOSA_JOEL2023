fb53999f089608230677bfe25f3dbdc50926aeec
JoelGlez <joelglez0106@gmail.com>
2023-10-22 14:24:40 -0600

    actualizado

---
+++ losprincipales.java
@@ -0,0 +1,16 @@
+public class losprincipales {
+    public static void main ( String[] args){
+
+         perros  guao= new perro("lalo", "pittbull", "carne", 4, 1);
+guao.mostrarperros();
+ hamsters  kiki= new hamsters("pedro", "dorado", "melon", 1, 1);
+kiki.mostrarhamsters();
+ conejos  gui= new conejo("roco", "mini lop", "zanahoria", 2, 1);
+gui.mostrarconejos();
+cuyos titi = new cuyo("creiss", "cobaya", "heno", 6 , 1);
+titi.mostrarcuyos();
+ urones  ghss= new urones("flopy", "bull", "carne", 1, 1);
+ghss.mostrarurones();
+
+}
+}
